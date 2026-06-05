33540cf66a561ebcd4f8b9dfc57e23a816b7f517
luiscruzz.developer <luiscruzz.developer@gmail.com>
2026-06-05 12:15:30 -0300

    fix(backoffice): restaurar config.AUTHORIZATION (lo usa utils/connection.ts; rompia el login)

--- backoffice/src/config/config.ts
+++ backoffice/src/config/config.ts
@@ -1,5 +1,9 @@
 const config = {
     basePath: '',
+    AUTHORIZATION: {
+        USER: process.env.AUTH_BASIC_USER,
+        PASSWORD: process.env.AUTH_BASIC_PW,
+    },
     AUTH_API_URL: process.env.AUTH_API_URL,
 };
 
